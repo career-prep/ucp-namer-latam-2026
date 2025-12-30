e7fcd4a43c28d21dca1306636441a01959479ec7
OlUwAtOmI06 <oluwatomiadenekan@gmail.com>
2025-12-29 21:47:37 -0800

    Add zero-sum pair counting function
    Implement function to count zero-sum pairs in an array.

--- tomi_adenekan_0/q0_ZeroSum.py
+++ tomi_adenekan_0/q0_ZeroSum.py
@@ -1,1 +1,17 @@
+def main(array):
+    array.sort()
+    dic = {}
+    for each in array:
+        if each in dic:
+            dic[each] += 1
+        else:
+            dic[each] = 1
+    res = 0
+    for key, value in dic.items():
+        if -key in dic and dic[key] == dic[-key]:
+            res += (0.5  * value)
 
+    return int(res)
+
+lst = [1, 10, 8, 3, 2, 5, 7, 2, -2, -1]
+print(main(lst))
