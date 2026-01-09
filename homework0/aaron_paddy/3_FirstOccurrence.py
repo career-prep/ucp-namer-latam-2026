3b26364364d65dc42ed629291d5bd2b42c0ab006
Aaron Paddy <aaronpaddy@MacBook-Air.local>
2026-01-09 17:00:34 -0600

    Added homework0 solutions and README

---
+++ homework0/aaron_paddy/3_FirstOccurrence.py
@@ -0,0 +1,25 @@
+#SPACE COMPLEXITY: 0(N) TIME COMPLEXITY: 0(N)
+
+from collections import defaultdict
+def first_occurrence(word):
+    dic = defaultdict(int)
+    new_word = ""
+    for n in word:
+        dic[n] += 1
+    for key in dic:
+        new_word += key    
+    return new_word
+
+
+def test_cases():
+    assert first_occurrence("") == ""
+    assert first_occurrence("Banana banana") == "Ban b"
+    assert first_occurrence("streets") == "stre"
+    assert first_occurrence("s") == "s"
+    
+if __name__ == "__main__":
+    test_cases()
+    print("All test cases passed successfully")
+    
+#TIME SPENT = 18 minutes
+    
