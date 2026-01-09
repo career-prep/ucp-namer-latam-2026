74785e351b8a1cd8475a4b53a9330e63ecb4ad6f
xo4i-11 <nghiemanhduy1308@gmail.com>
2026-01-09 17:23:53 -0500

    question 2 done

--- homework0/duy_nghiem/q2_FirstOccurrence.py
+++ homework0/duy_nghiem/q2_FirstOccurrence.py
@@ -1,1 +1,29 @@
-Empty question 2 file
+# Question 3: First Occurrence
+# Given a string => return a string that contains the first occurrence of each char
+
+# Approach:
+# create a set that stores the unique elements
+# if elem not in the set, add to the string and add them in the set
+
+# => Spend 5 mins
+
+# Implementation:
+
+def first_occurance(s):
+    res=""
+    unique_char=set()
+
+    for c in s:
+        if c not in unique_char:
+            res+=c
+            unique_char.add(c)
+            
+    return res
+
+# test case:
+print(first_occurance("abracadabra"))
+print(first_occurance("Uber Career Prep"))
+print(first_occurance("zzyzx"))
+
+
+ 
