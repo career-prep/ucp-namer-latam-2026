7386ee5b151e3595dc557ef3eea9b99fa200aef9
Danny Tate <dannytxt77@email.com>
2026-01-13 00:27:57 -0800

    Homework 0 completed with README

--- homework0/danny_tat/q2_UniqueSum.py
+++ homework0/danny_tat/q2_UniqueSum.py
@@ -1,1 +1,27 @@
+"""
+Given an array of integers, return the sum of unique elements in the array
 
+Example: 
+Input: [1, 10, 8, 3, 2, 5, 7, 2, -2, -1]
+Expected Output: 33
+
+Input: [4, 3, 3, 5, 7, 0, 2, 3, 8, 6]
+Expected Output: 35
+"""
+
+
+def unique_sum(num):
+    my_map = {}
+    total = 0
+
+    for number in num:
+        my_map[number] = my_map.get(number, 0) + 1
+
+        if my_map.get(number) == 1:
+            total += number
+
+    return total
+
+
+print(unique_sum([1, 10, 8, 3, 2, 5, 7, 2, -2, -1]))
+print(unique_sum([4, 3, 3, 5, 7, 0, 2, 3, 8, 6]))
