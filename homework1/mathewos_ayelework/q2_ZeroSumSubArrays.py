32cb6513e8472b3a70f648f50327a4e48f1c62e1
mathew2005 <mathewoswondwosen2500@gmail.com>
2026-01-26 02:01:15 -0500

    q4 attempt #1 (not finished yet)

--- homework1/mathewos_ayelework/q2_ZeroSumSubArrays.py
+++ homework1/mathewos_ayelework/q2_ZeroSumSubArrays.py
@@ -1,4 +1,11 @@
-
+def zeroSumSubArray(nums: list[int]) -> int:
+    # iterate through the list
+    counter = 0
+    size = 1
+    for i in range(len(nums)):
+        for j in range(i, len(nums)):
+            pass
+        size += 1 
 # Time Complexity:
 # Space Complexity:
-# Time Taken:
+# Time Taken: 13 mins
