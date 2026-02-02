e62feb0844466e0d01030fde979b4a195b6c62c0
OlUwAtOmI06 <oluwatomiadenekan@gmail.com>
2026-02-02 15:36:41 -0800

    Introduce Total variable for summing list elements
    Added a Total variable to accumulate the sum of the list elements.

--- tomi_adenekan_1/question3.py
+++ tomi_adenekan_1/question3.py
@@ -5,15 +5,18 @@ brute force
 
 def get_name(lst):
     count = 0
+    Total = 0
     for i in range(len(lst)):
+        Total += lst[i]
+        check = Total
         for j in range(i):
-            if sum(lst[j:i+1]) == 0:
+            check -= lst[j]
+            if check == 0:
                 count += 1
                 
 
 
     return count
-
 
 lst = [4,5,2,-1,-3,-3,4,6,-7]
 
