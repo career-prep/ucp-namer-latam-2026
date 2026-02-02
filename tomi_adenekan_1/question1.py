1170798581d21c34d9ba24975eb3698b9e126a22
OlUwAtOmI06 <oluwatomiadenekan@gmail.com>
2026-02-02 13:45:02 -0800

    Implement fixed-size sliding window function

---
+++ tomi_adenekan_1/question1.py
@@ -0,0 +1,19 @@
+"""
+fixed-size sliding window
+10 minuutes
+"""
+
+def get_name(lis, k):
+    val = 0
+   
+    for i in range(len(lis)-k+1):
+        total = sum(lis[i:i+k])
+        val = max(val, total/k)
+
+
+    return val
+
+lis = [1,1,1,1,-1,-1,2,-1,-1,6]
+k = 5
+
+print(get_name(lis, k))
