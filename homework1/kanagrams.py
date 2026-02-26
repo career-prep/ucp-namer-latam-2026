51453aea3f78413f454b14f962aa7c76c394e5ea
Prashant Banjade <banjade@Prashants-MacBook-Pro.local>
2026-02-26 01:11:16 -0600

    Complete K-Anagrams problem solution

--- homework1/kanagrams.py
+++ homework1/kanagrams.py
@@ -17,4 +17,29 @@
 # Input k: 2
 # Output: False
 
+# notes- 
+# time complexity - o(N) as it is travelling 1 times in each array
+# time taken- around 40 mins
+# 2 string increase/ decrease hashmap count
+
+def kanagrams(str1, str2, k):
+    if len(str1) != len(str2):
+        return False
+    hasmap= {}
+    for ch in str1:
+        if ch in hasmap:
+            hasmap[ch] += 1 
+        else:
+            hasmap[ch] = 1
+    
+    for ch in str2:
+        if ch in hasmap:
+            hasmap[ch] -= 1 
+        
+    total = sum(hasmap.values())
+
+    return total <= k 
+print(kanagrams("apple", "peach", 1))
+print(kanagrams("debit curd", "bad credit", 1))
+
 
