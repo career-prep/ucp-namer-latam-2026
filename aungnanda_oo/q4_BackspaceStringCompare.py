9f1ca0d8ff480230832d4da832b1f737f4b166e2
Aung Nanda Oo <aungnandaoo.425@gmail.com>
2026-02-26 12:33:28 -0800

    address feedback for backspace string

--- aungnanda_oo/q4_BackspaceStringCompare.py
+++ aungnanda_oo/q4_BackspaceStringCompare.py
@@ -52,3 +52,62 @@ for test_case in test_cases:
 # Space Complexity = O(n)
 
 # Spent a total of 40 mins on this question
+
+# Feedback addressed by Aung for Eri
+def BackspaceStringCompare(s, t):
+    i = len(s) - 1
+    j = len(t) - 1
+    skip_s = 0
+    skip_t = 0
+
+    while i >= 0 or j >= 0:
+
+        # find next valid char in s
+        while i >= 0:
+            if s[i] == '#':
+                skip_s += 1
+                i -= 1
+            elif skip_s > 0:
+                skip_s -= 1
+                i -= 1
+            else:
+                break  # found valid char
+
+        # find next valid char in t
+        while j >= 0:
+            if t[j] == '#':
+                skip_t += 1
+                j -= 1
+            elif skip_t > 0:
+                skip_t -= 1
+                j -= 1
+            else:
+                break  # found valid char
+
+        # compare current valid chars
+        if i >= 0 and j >= 0:
+            if s[i] != t[j]:
+                return False
+        elif i >= 0 or j >= 0:
+            # one string still has chars, other is exhausted
+            return False
+
+        i -= 1
+        j -= 1
+
+    return True
+
+
+test_cases = [
+    ("abcde", "abcde"),
+    ("Uber Career Prep", "u#Uber Careee#r Prep"),
+    ("abcdef###xyz", "abcw#xyz"),
+    ("abcdef###xyz", "abcdefxyz####")
+]
+
+for s, t in test_cases:
+    print(BackspaceStringCompare(s, t))
+
+
+# Time Complexity = O(n)
+# Space Complexity = O(1)
