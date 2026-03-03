3792e000ccaf49159454979aacb088a09b28b864
Avi161 <paudela@union.edu>
2026-03-03 09:58:03 -0500

    Add optimized space solution for q9

--- homework1/avigya_paudel/q9_dedup_array.py
+++ homework1/avigya_paudel/q9_dedup_array.py
@@ -1,13 +1,21 @@
 # TIME COMPLEXITY: O(N) 
-# SPACE COMPEXITY: O(N)
+# SPACE COMPEXITY: O(1)
 # TIME TAKEN: ~3 minutes
-# TECHNIQUE: Note sure
+# TECHNIQUE: Reset/catch-up two pointer
 
 def dedup_arr(arr):
     """
     returns a array with no duplicates from a sorted array
     """
-    return list(set(arr))
+    if not arr:
+        return []
+    
+    slow = 0
+    for fast in range(1, len(arr)):
+        if arr[slow] != arr[fast]:
+            slow += 1
+            arr[slow] = arr[fast]
+    return arr[:slow+1]
 
 if __name__ == "__main__":
     print(dedup_arr([1,2,2,3,3,4,4,4,4]))
