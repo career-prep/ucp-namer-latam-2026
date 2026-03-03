0c6f57d002887e44ba97d3c2f919d24c360b84c3
Avi161 <paudela@union.edu>
2026-03-03 08:31:27 -0500

    Optimize space for q2

--- homework1/avigya_paudel/q2_reverse_vowels.py
+++ homework1/avigya_paudel/q2_reverse_vowels.py
@@ -1,40 +1,35 @@
 # TIME COMPLEXITY: O(N) 
-# SPACE COMPEXITY: O(N)
+# SPACE COMPEXITY: O(1)
 # TIME TAKEN: ~7 minutes
 # TECHNIQUE: Not sure if it aligns with any
+
+from turtle import left
+
 
 def reverse_vowels(input_str):
     """
     returns a string with everything the same as input_str
     except index of vowels are reversed
     """
+    vowels = set("AEIOUaeiou")
+    input_str = list(input_str)
+    left, right = 0, len(input_str)-1
 
-    vowels = {"a", "e", "i", "o", "u"}
-    vowel_arr = collect_vowels(input_str, vowels)
-    vowel_arr.reverse()
-    vowel_idx = 0
-    
-    res = ""
-    for c in input_str:
-        if c.lower() in vowels:
-            res += vowel_arr[vowel_idx] 
-            vowel_idx += 1 
+    while left < right:
+        if input_str[left] not in vowels:
+            left += 1
+        elif input_str[right] not in vowels:
+            right -= 1
         else:
-            res += c
-    return res
+            input_str[left], input_str[right] = input_str[right], input_str[left]
+            left += 1
+            right -= 1
+    
+    return "".join(input_str)
 
-def collect_vowels(str, vowels):
-    """
-    returns an array of all the vowels in the sequence from left to right
-    """
-    ans = []
-    for chr in str:
-        if chr.lower() in vowels:
-            ans.append(chr)
-    return ans 
 
 if __name__ == "__main__":
-    print(reverse_vowels("Uber Career Prep"))
-    print(reverse_vowels("xyz"))
-    print(reverse_vowels("flamingo"))
+    print(reverse_vowels("Uber Career Prep")) # expected: eber Ceraer PrUp
+    print(reverse_vowels("xyz")) # xyz
+    print(reverse_vowels("flamingo")) # flominga
     
