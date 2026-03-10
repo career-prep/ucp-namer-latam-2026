aa06c38882efc058ebdc56d21958479760cf4c53
Aman <aman196@gmail.com>
2026-03-09 21:06:00 -0700

    redid q5 properly

--- homework1/aman_varshney/q5_ShortestSubstring.py
+++ homework1/aman_varshney/q5_ShortestSubstring.py
@@ -1,60 +1,51 @@
-# dynamic sliding window
-# spent 40 min
-# Time complexity - O(n^2*k) 
-# Space Complexity - O(n)
-
-from math import inf
+# sliding window
+# Time complexity: O(n+m) 
+# Space complexity: O(k) 
+# where n is length of s1 and m is length of s2 and k is number of unique chars in s2
 
 
 def shortestSubstring(s1, s2):
     # empty case
     if not s1 or not s2: 
         return -1
-    
+
     # load frequency hashmap of required letters and get length
-    target_freq = {} 
-    len_s2 = 0 
+    target_freq = {}
     for char in s2:
         target_freq[char] = target_freq.get(char, 0) + 1
-        len_s2 += 1
+    len_s2 = len(s2)
     
     # ensure s1 is equal/longer
     len_s1 = len(s1)
-    if len_s1 < len_s2: 
+    if len_s1 < len_s2:
         return -1
     
-    shortest_len = inf # keeps track of shortest substring
-    
-    for left in range(len_s1-len_s2+1): # iterate through s1 with room for window of size s2
-        if s1[left] not in target_freq: # will not be shortest
-            continue
+    # sliding window
+    window_freq = {}
+    have, need = 0, len(target_freq)
+    shortest_len = float('inf')
+    left = 0
+    for right in range(len_s1):
+        char = s1[right]
+        if char in target_freq:
+            window_freq[char] = window_freq.get(char, 0) + 1
+            if window_freq[char] == target_freq[char]: # only increment when we accumulated enough of this char
+                have += 1
         
-        window_freq = {s1[left] : 1} # window frequency map
-        right = left + 1 # expands window
-        while (window_freq != target_freq):
-            if right == len_s1: # oob --> no more possible combos --> return shortest
-                return -1 if shortest_len == inf else shortest_len
+        while have == need: # valid window
+            shortest_len = min(shortest_len, right-left+1) # update shortest
+            left_char = s1[left]
+            if left_char in target_freq:
+                window_freq[left_char] -= 1
+                if window_freq[left_char] < target_freq[left_char]: # only decrement when we no longer have enough of this char
+                    have -= 1
+            left += 1
             
-            if s1[right] in target_freq: # in target string -> add to window
-                if s1[right] in window_freq:
-                    if window_freq[s1[right]] != target_freq[s1[right]]: # make sure to not exceed value or else check will fail
-                        window_freq[s1[right]] += 1
-                else:
-                    window_freq[s1[right]] = 1
-            
-            right += 1
-            
-        shortest_len = min(shortest_len, right-left) # update shortest
-        
-        if shortest_len == len_s2: # cannot be any shorter
-            return shortest_len
-        
-    return -1 if shortest_len == inf else shortest_len # -1 if pattern not found 
-        
+    return shortest_len if shortest_len != float('inf') else -1
 
-    
-        
-    
+
+
+
 if __name__ == "__main__":
     string1 = "abracadabraw"
     string2 = "braw"
