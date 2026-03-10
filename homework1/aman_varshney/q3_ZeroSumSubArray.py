4e3b9be718f3ea348343d8e734cf97a9e40c9b40
Aman <aman196@gmail.com>
2026-03-09 23:05:07 -0700

    used prefix sums

--- homework1/aman_varshney/q3_ZeroSumSubArray.py
+++ homework1/aman_varshney/q3_ZeroSumSubArray.py
@@ -1,26 +1,22 @@
-# variable size sliding window
-# Time Complexity - O(n^2)
-# Space Complexity - O(1)
-# couldve used hashmap
+# one directional running computation
+# Time Complexity - O(n)
+# Space Complexity - O(n)
 
 
 def zeroSumCount(arr):
+    sum = 0 
     count = 0
-    for left in range(len(arr)-1):
-        sum = arr[left]
+    freq = {0: 1}
+    
+    for num in arr:
+        sum += num
+        if sum in freq:
+            count += freq[sum] # add count of previous occurrences of this prefix sum
+        freq[sum] = freq.get(sum, 0) + 1 # update frequency of current prefix sum
         
-        if sum == 0: # balanced
-            count += 1
-            
-        for right in range(left+1, len(arr)):
-            sum += arr[right]
-            
-            if sum == 0:
-                count += 1
-                
     return count
-
-
+    
+    
 
 if __name__ == "__main__":
     arr1 = [4,5,2,-1,-3,-3,4,6,-7]
@@ -39,33 +35,3 @@ if __name__ == "__main__":
     out3 = zeroSumCount(arr3)
     print("test 3 -- ", out3)
     
-
-   
-# spent 40 minutes 
-    
-
-'''
-def zeroSum(arr): 
-    list_arrays = [] # will store balanced subarrays
-    
-    for start in range(len(arr)): 
-        subarray = [arr[start]] # tracks numbers
-        sum = arr[start] # tracks sum of subarray
-        
-        if sum == 0: # balanced --> add number as a subarray
-            list_arrays.append(subarray)
-            
-            
-        for end in range(start+1, len(arr)): # check rest of array for balanced subarray
-            subarray.append(arr[end])
-            sum += arr[end]
-            
-            if (sum == 0): # balanced --> add subarray 
-                l = []
-                for item in subarray: # had to do this for some reason instead of appending subarray
-                    l.append(item)
-                list_arrays.append(l)
-                
-        
-    return list_arrays
-'''
