213285fd97533ef8af71727253a5923e27aa4e22
OlUwAtOmI06 <oluwatomiadenekan@gmail.com>
2026-03-24 12:51:17 -0700

    Add doubly linked list implementation
    Implement a doubly linked list with various methods including insertion, deletion, and reversal.

---
+++ tomi_adenekan2/Doubly_Linked_List.py
@@ -0,0 +1,124 @@
+"""40 minutes
+Oluwatomi
+"""
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
+
+class List:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+    def insertAtFront(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+        if self.tail is None:
+            self.tail = new_node
+
+    def insertAtBack(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.insertAtFront(data)
+            return
+        new_node.prev = self.tail
+        self.tail.next = new_node
+        self.tail = new_node
+    
+    def insertAfter(self, data, location):
+        new_node =  Node(data)
+        new_node.prev = location
+        new_node.next =  location.next
+        if location.next:
+            location.next.prev = new_node
+        else:
+            self.tail = new_node
+        location.next = new_node
+        
+        return
+
+    def insertBefore(self, data, location):
+        new_node = Node(data)
+        if self.head == location:
+            self.insertAtFront(data)
+            return
+        new_node.next = location
+        new_node.prev = location.prev
+        location.prev.next = new_node
+        location.prev = new_node
+
+
+    def deleteFront(self):
+        if self.head is None:
+            return
+        if self.head == self.tail:
+            self.head = None
+            self.tail = None
+            return
+
+        cur = self.head.next
+        cur.prev = None
+        self.head = cur
+    def deleteBack(self):
+        if self.head is None:
+            return
+        if self.head == self.tail:
+            self.head = None
+            self.tail = None
+            return
+        cur = self.tail.prev
+        cur.next = None
+        self.tail = cur
+    
+    def deleteNode(self, location):
+        if self.head == location:
+            self.deleteFront()
+            return
+        if self.tail == location:
+            self.deleteBack()
+            return
+        location.prev.next = location.next
+        location.next.prev = location.prev
+
+
+    def length(self):
+        count = 0
+        cur = self.head
+        while cur:
+            count += 1
+            cur = cur.next
+        return count
+    def reverseIterative(self):
+        if self.head is None:
+            return
+        cur = self.head
+        prev = None
+        while cur:
+            temp = cur.next
+            cur.next = prev
+            prev = cur
+            cur = temp
+        self.head, self.tail = self.tail, self.head
+
+    def print_list(self):
+        current = self.head
+        while current:
+            print(current.data,  end=" <-> ")
+            current = current.next
+        print("None")
+
+
+
+
+my_list = List()
+my_list.insertAtFront(10)
+my_list.insertAtFront(13)
+my_list.print_list()
+my_list.insertAtBack(15)
+my_list.print_list()
+print(my_list.length())
+my_list.reverseIterative()
+my_list.print_list()
