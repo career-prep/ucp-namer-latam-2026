a59da3d84fdcadc25f624bc741aecf213e9b4e1b
rishavcacharya9 <rishavcacharya@gmail.com>
2026-03-23 21:02:20 -0500

    Update q7_CopyTree.py

--- rishav_acharya_hw_2/q7_CopyTree.py
+++ rishav_acharya_hw_2/q7_CopyTree.py
@@ -1,1 +1,28 @@
+# 29 minutes
+# Generic tree traversal technique
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
 
+def copyTree(root):
+    if root is None:
+        return None
+
+    newroot = Node(root.data)
+
+    newroot.left = copyTree(root.left)
+    newroot.right = copyTree(root.right)
+
+    return newroot
+
+if __name__ == "__main__":
+    root = Node(1)
+    root.left = Node(2)
+    root.right = Node(3)
+
+    copied = copyTree(root)
+    print(root is copied)
+    print(root.left is copied.left)
+    print(root.data, copied.data)
