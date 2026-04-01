c304529ca91c29df0bb09a9788159ac41d339789
OlUwAtOmI06 <oluwatomiadenekan@gmail.com>
2026-04-01 10:59:57 -0700

    Fix node initialization in PushFront and PushBack

--- tomi_adenekan2/Deque.py
+++ tomi_adenekan2/Deque.py
@@ -30,14 +30,16 @@ class Deque:
         new_node.next = self.head
         if self.head:
             self.head.prev = new_node
+        self.head = new_node
         if self.tail is None:
             self.tail = new_node
 
     def PushBack(self, data):
-        new_node = Node(data)
+        
         if self.head is None:
             self.PushFront(data)
             return
+        new_node = Node(data)
         new_node.prev = self.tail
         self.tail.next = new_node
         self.tail = new_node
