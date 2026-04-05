5d37d2ae8b90b53c4bdf75479b792b5c156d04f5
Aman <aman196@gmail.com>
2026-04-04 17:50:39 -0700

    main for queue

--- homework2/aman_varshney/q4_Queue.py
+++ homework2/aman_varshney/q4_Queue.py
@@ -42,4 +42,42 @@ class Queue:
         
     def isEmpty(self):
         return (self.head is None)
-            
+
+
+if __name__ == "__main__":
+    # 1
+    # empty queue
+    q = Queue()
+    assert q.isEmpty() is True, "empty isEmpty error"
+    assert q.peek() is None, "empty peek error"
+    assert q.dequeue() is None, "empty dequeue error"
+
+
+    # 2
+    # enqueue / FIFO order
+    q.enqueue(1)
+    q.enqueue(2)
+    q.enqueue(3)
+    
+    assert q.isEmpty() is False, "nonempty isEmpty error"
+    assert q.peek() == 1, "peek error"
+    assert q.head.data == 1, "head error1"
+    assert q.tail.data == 3, "tail error1"
+    assert q.dequeue() == 1, "dequeue 1 error"
+    
+    assert q.peek() == 2, "peek after dequeue error1"
+    assert q.head.data == 2, "head error2"
+    assert q.tail.data == 3, "tail error2"
+    assert q.dequeue() == 2, "dequeue 2 error"
+    
+    assert q.peek() == 3, "peek after dequeue error2"
+    assert q.head.data == 3, "head error3"
+    assert q.tail.data == 3, "tail error3"
+    assert q.dequeue() == 3, "dequeue 3 error"
+    
+    assert q.peek() is None, "empty peek after dequeue error"
+    assert q.isEmpty() is True, "drained isEmpty error"
+    assert q.dequeue() is None, "dequeue empty again error"
+
+
+    print("All tests passed")
