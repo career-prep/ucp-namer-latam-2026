159bab54b0f0887599b1b9263cfcd482ce7137ab
Aman <aman196@gmail.com>
2026-05-01 17:58:42 -0700

    stack implementation included for q7

--- homework3/aman_varshney/q7_ReverseWords.py
+++ homework3/aman_varshney/q7_ReverseWords.py
@@ -1,9 +1,9 @@
-# spent 10 minutes
-# can use stack but i like delimiter better
+# spent 15 minutes
+# stack but i like delimiter better
 # TC - O(n) 
 # SC - O(n)
 
-def reverseWords(s: str) -> str:
+def reverse_words(s: str) -> str:
     if not s: # empty case
         return ""
     
@@ -18,15 +18,43 @@ def reverseWords(s: str) -> str:
     return res
 
     
+def reverse_words_stack(s: str) -> str:
+    if not s: 
+        return ""
+    
+    stack = []
+    word = ""
+    
+    # push words into stack
+    for c in s:
+        if c == " ":
+            if word:
+                stack.append(word)
+                word = ""
+        else:
+            word += c
+            
+    if word: # last word
+        stack.append(word)
+        
+    # pop from stack to reverse
+    res = []
+    while stack:
+        res.append(stack.pop())
+        
+    res_str = res[0] # already checked for empty case
+    for i in range(1, len(res)):
+        res_str += " " + res[i]
+    return res_str
     
     
 if __name__ == "__main__":
     s1 = "Uber Career Prep"
     print("Expected: Prep Career Uber")
-    print("Actual  :", reverseWords(s1))
+    print("Actual  :", reverse_words_stack(s1))
     
     s2 = "Emma lives in Brooklyn, New York."
     print("Expected: York. New Brooklyn, in lives Emma")
-    print("Actual  :", reverseWords(s2))
+    print("Actual  :", reverse_words_stack(s2))
 
     
