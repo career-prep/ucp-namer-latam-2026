e50a181683b7dafe6a35f1ff5bc90dcb880e4b1b
OlUwAtOmI06 <oluwatomiadenekan@gmail.com>
2026-05-01 22:43:05 -0700

    Implement basic heap class with insert and remove methods

---
+++ tomi_adenekan_3/Question2.py
@@ -0,0 +1,34 @@
+"""
+[5, 3, 8, 1, 2]
+    3
+5       8
+
+"""
+class heap:
+    def __init__(self):
+        self.array = []
+        self.count = 0
+    def top(self):
+        if self.count == 0:
+            return None
+        return self.array[0]
+    def insert(self, val):
+        """if self.count == 0:
+            self.array.append(val)
+            
+        else:"""
+        self.array.append(val)
+        idx = self.count 
+        while idx > 0:
+            parent = idx // 2
+            
+            if self.array[idx] < self.array[parent]:
+                self.array[idx], self.array[parent] = self.array[parent], self.array[idx]
+                idx = parent
+            else:
+                break
+        self.count += 1
+    def remove(self):
+        if self.count > 0:
+            self.count -= 1
+            self.array.pop(0)
