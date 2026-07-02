b3eac5ba9c4c8cf1d86651b7ff5f162f057b3770
Samaksh Arora <samaksharora.09@gmail.com>
2026-07-02 09:45:49 -0400

    minor changes

--- homework4/samaksh_arora/q5_minCostStairClimbing.py
+++ homework4/samaksh_arora/q5_minCostStairClimbing.py
@@ -2,7 +2,7 @@
 #Question 5 - Min Cost Stair Climbing
 #Dynamic programming (Tabulation)
 #Time Complexity: O(n) where n is the number of stairs
-#Space Complexity: O(n)
+#Space Complexity: O(1)
 #Time Spent: 33 minutes
 
 
@@ -14,21 +14,20 @@ def minCostStairClimbing(costs):
     elif n == 1:
         return costs[0]
 
-    # Create a table to store the minimum cost to reach each step
-    dp = [0] * n
-    dp[0] = costs[0]
-    dp[1] = costs[1]
+    prev2 = costs[0]
+    prev1 = costs[1]
 
     for i in range(2, n):
-        dp[i] = costs[i] + min(dp[i - 1], dp[i - 2])
+        current = costs[i] + min(prev1, prev2)
+        prev2 = prev1
+        prev1 = current
 
-    # The minimum cost to reach the top of the stairs is the minimum of the last two entries
-    return min(dp[-1], dp[-2])
+    return min(prev1, prev2)
 
 #Test Cases
 assert minCostStairClimbing([4, 1, 6, 3, 5, 8]) == 9
 assert minCostStairClimbing([11, 8, 3, 4, 9, 13, 10]) == 25
 
-# Extra: 
-assert minCostStairClimbing([1, 2, 3, 4, 5]) == 8
+# Extra:
+assert minCostStairClimbing([1, 2, 3, 4, 5]) == 6
 assert minCostStairClimbing([10]) == 10
